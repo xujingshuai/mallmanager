8dc305bd3fbb95806278c2f187556de7f469e15e
xujingshuai <15128654212@163.com>
2018-11-27 15:40:37 +0800

    获取用户列表数据

--- src/components/login/login.vue
+++ src/components/login/login.vue
@@ -1,16 +1,28 @@
 <template>
-    <div class="login-wrap">
-        <el-form class="login-form" label-position="top" label-width="80px" :model="formData">
-            <h2>用户管理</h2>
-            <el-form-item label="用户名">
-                <el-input v-model="formData.username"></el-input>
-            </el-form-item>
-            <el-form-item label="密码">
-                <el-input v-model="formData.password"></el-input>
-            </el-form-item>
-            <el-button class="login-button" type="primary" @click.prevent="handleLogin()">登录</el-button>
-        </el-form>
-    </div>
+  <div class="login-wrap">
+    <el-form
+      class="login-form"
+      label-position="top"
+      label-width="80px"
+      :model="formData"
+    >
+      <h2>用户管理</h2>
+      <el-form-item label="用户名">
+        <el-input v-model="formData.username"></el-input>
+      </el-form-item>
+      <el-form-item label="密码">
+        <el-input
+          @keyup.enter.native="handleLogin()"
+          v-model="formData.password"
+        ></el-input>
+      </el-form-item>
+      <el-button
+        class="login-button"
+        type="primary"
+        @click.prevent="handleLogin()"
+      >登录</el-button>
+    </el-form>
+  </div>
 </template>
 
 <script>
@@ -24,8 +36,10 @@ export default {
         }
     },
     methods: {
-        handleLogin() {
-            this.$http.post('/login',this.formData).then((res) => {
+        async handleLogin() {
+            const res = await this.$http.post('/login',this.formData);
+            
+            // console.log(res);
                 const {data, meta: {msg, status}} = res.data;
                 if(status === 200) {
                     // 存储token值
@@ -34,11 +48,9 @@ export default {
                     this.$message.success(msg);
                     // 登录成功，跳转到home页
                     this.$router.push({name: 'home'});
-
                 } else {
                      this.$message.warning(msg);
                 }
-            })
         }
     }
 };
--- src/components/users/users.vue
+++ src/components/users/users.vue
@@ -1,13 +1,102 @@
 <template>
-    <div>用户</div>
+  <el-card class="card">
+    <!-- 面包屑组件 -->
+    <el-breadcrumb separator-class="el-icon-arrow-right">
+      <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
+      <el-breadcrumb-item>用户管理</el-breadcrumb-item>
+      <el-breadcrumb-item>用户列表</el-breadcrumb-item>
+    </el-breadcrumb>
+    <!-- 搜索 -->
+    <el-row class="searchRow">
+        <el-col :span="24">
+            <el-input placeholder="请输入内容" clearable class="searchInput">
+                <el-button slot="append" icon="el-icon-search"></el-button>
+            </el-input>
+            <el-button type="success" plain>添加用户</el-button>
+        </el-col>
+    </el-row>
+    <!-- 表格 -->
+    <el-table
+      :data="data"
+      style="width: 100%"
+      stripe
+      border>
+      <!-- 序号 -->
+      <el-table-column
+      type="index"
+      width="30">
+    </el-table-column>
+      <el-table-column
+        prop="username"
+        label="姓名"
+        width="180">
+      </el-table-column>
+      <el-table-column
+        prop="email"
+        label="邮箱"
+        width="180">
+      </el-table-column>
+      <el-table-column
+        prop="mobile"
+        label="电话"
+        width="180">
+      </el-table-column>
+      <el-table-column
+        prop="mg_state"
+        label="用户状态"
+        width="90">
+      </el-table-column>
+      <el-table-column
+        label="操作"
+        >
+      </el-table-column>
+    </el-table>
+  </el-card>
 </template>
 
 <script>
 export default {
-
+    data() {
+        return {
+          query: '',
+          pagenum: 1,
+          pagesize: 10,
+          data: []
+        }
+      },
+      
+      created() {
+          this.loadData();
+      },
+      methods: {
+          async loadData() {
+            //   发送请求的时候，要在请求头中添加Authorization = token
+            const AUTH_TOKEN = localStorage.getItem('token');
+            this.$http.defaults.headers.common['Authorization'] = AUTH_TOKEN;
+            const res = await this.$http.get(`users?query=${this.query}&pagenum=${this.pagenum}&pagesize=${this.pagesize}`);
+            console.log(res);
+            // const {meta: {status, msg}, data} = res.data;
+            // if(status === 200) {
+            //     this.data = res.data.data.users;
+            // } else {
+            //     this.$message.error(msg);
+            // }
+          }
+      }
+    
 }
 </script>
 
 <style>
+    .card {
+        height: 100%;
+    }
 
+    .searchRow {
+        margin: 10px 0;
+    }
+
+    .searchInput {
+        width: 260px;
+    }
 </style>
